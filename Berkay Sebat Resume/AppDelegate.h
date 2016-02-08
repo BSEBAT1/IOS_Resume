ceaf94fff754a60161a56656fae23ed23d039db2
macbook pro <macbookpro@macbooks-macbook-pro.local>
2016-02-07 22:44:13 -0500

    Initial Commit

---
+++ Berkay Sebat Resume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Berkay Sebat Resume
+//
+//  Created by macbook pro on 2/7/16.
+//  Copyright © 2016 berkaysebat.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Berkay Sebat Resume/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Berkay Sebat Resume
+//
+//  Created by macbook pro on 2/7/16.
+//  Copyright © 2016 berkaysebat.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
